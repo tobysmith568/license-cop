60ed21a9bb2c97fb98263f516ccfa81ff472c771
Toby Smith <tobysmith568@hotmail.co.uk>
2024-09-29 23:40:38 +0100

    Switch to a typescript astro config

--- apps/website/astro.config.ts
+++ apps/website/astro.config.ts
@@ -1,19 +1,16 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import webmanifest, { WebmanifestOptions } from "astro-webmanifest";
+import webmanifest, { type WebmanifestOptions } from "astro-webmanifest";
 import { defineConfig } from "astro/config";
 import { dirname } from "path";
 import directive from "remark-directive";
 import { fileURLToPath } from "url";
-import admonitionsPlugin from "./plugins/admonitions.mjs";
+import admonitionsPlugin from "./plugins/admonitions";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-/**
- * @type {WebmanifestOptions}
- */
-const webmanifestOptions = {
+const webmanifestOptions: WebmanifestOptions = {
   name: "License-Cop",
   icon: __dirname + "/src/images/favicon.svg",
   short_name: "License-Cop",
